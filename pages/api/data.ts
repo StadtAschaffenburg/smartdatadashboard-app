4973e314c6457a30590a8227c641df1a2a5ffa6b
Fabian Prinz-Arnold <prinz-arnold@schleegleixner.de>
2024-08-01 10:24:48 +0200

    fix: added next cors

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { client } from '@/lib/cms'
+import NextCors from 'nextjs-cors';
 
 interface DataResponse {
   content?: string;
@@ -10,6 +11,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   const lifetime = parseInt(req.query.lifetime as string, 10);
 
   const data = await client.getAPI(route, true, lifetime);
+
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+  });
   
   if (data) {
     res.status(200).json(data);
