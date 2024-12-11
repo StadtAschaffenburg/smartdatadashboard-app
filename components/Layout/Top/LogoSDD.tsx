72b32eda27c323a55c5cfd51075120428174f60d
Fabian Prinz-Arnold <prinz-arnold@schleegleixner.de>
2024-12-11 09:46:47 +0100

    feat: optimized logo size

--- components/Layout/Top/LogoSDD.tsx
+++ components/Layout/Top/LogoSDD.tsx
@@ -3,7 +3,7 @@ import SDDLogo from '@/assets/logos/logo_sdd.svg'
 
 export default function LogoSSD() {
   return (
-    <div className="flex h-32 w-60 items-center justify-center rounded bg-white p-4 md:h-40">
+    <div className="flex h-32 w-64 items-center justify-end bg-white p-4 pr-0 md:h-40 md:w-80">
       <Image
         alt="Logo des SmartDataDashboards Aschaffenburg"
         className="pointer-events-none ml-auto w-full"
