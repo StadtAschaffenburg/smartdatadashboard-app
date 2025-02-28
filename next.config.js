54412fb3990ffa958153641b3fa527916943207a
Fabian Prinz-Arnold <prinz-arnold@schleegleixner.de>
2025-02-28 11:54:43 +0100

    feat: added PWA option

--- next.config.js
+++ next.config.js
@@ -27,4 +27,9 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+})
+
+module.exports = withPWA(nextConfig)
